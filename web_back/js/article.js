eb5df5eb63f627746398484e0e528b40751393a9
cmy <1732728869@qq.com>
2019-12-04 17:05:12 +0800

    发表文章完结

--- web_back/js/article.js
+++ web_back/js/article.js
@@ -1,8 +1,23 @@
 var urll = 'http://localhost:8000/';
 var article ={
+    //查询
     get :function(data,callback) {
         $.get(urll +"admin/search",data,function(res) {
             callback(res);
+        })
+    },
+
+    //发布文章
+    add: function (fd,callback) {
+        $.ajax({
+            type:'post',
+            url:urll+'admin/article_publish',
+            data: fd,
+            success:function(res) {
+                callback(res)
+            },
+            processData:false,
+            contentType:false
         })
     }
 }
