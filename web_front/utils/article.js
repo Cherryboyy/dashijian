029053f82fc042a5dc9e732b92aa33cb76ac7712
cmy <1732728869@qq.com>
2019-12-05 12:00:55 +0800

    用户页分类，图片，最新资讯完成

---
+++ web_front/utils/article.js
@@ -0,0 +1,12 @@
+var article ={
+    hotpic : function(callback) {
+        $.get(apis.article_hotpic,function(res) {
+            callback(res)
+        })
+    },
+    latest :function(callback) {
+        $.get(apis.article_latest,function(res) {
+            callback(res)
+        })
+    }
+}
---
+++ web_front/utils/config.js
@@ -0,0 +1,10 @@
+var urll = 'http://localhost:8000'
+
+var apis = {
+    //分类相关接口
+    category_show: urll +'/category',
+    //文章相关接口
+    article_hotpic : urll +'/hotpic',
+    article_latest:urll +'/latest'
+    //评论接口
+}
